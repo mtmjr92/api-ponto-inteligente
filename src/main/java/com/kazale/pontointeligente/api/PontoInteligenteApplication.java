4b83b5a8c9d28678940e2ad6fd5e0b0cb365d2ae
mtmjr92 <>
2022-05-05 12:05:04 -0300

    23. Criando serviços (Service)

--- src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,6 +1,8 @@
 package com.kazale.pontointeligente.api;
 
+import com.kazale.pontointeligente.api.services.ExemploService;
 import com.kazale.pontointeligente.api.utils.SenhaUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +14,9 @@ public class PontoInteligenteApplication {
 
 	@Value("${paginacao.qtd_por_pagina}")
 	private int qntPorPagina;
+
+	@Autowired
+	private ExemploService exemploService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
@@ -31,6 +36,8 @@ public class PontoInteligenteApplication {
 			System.out.println("Senha encoded: " + senhaEncoded);
 
 			System.out.println("Senha valida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+
+			this.exemploService.testarServico();
 		};
 	}
 
---
+++ src/main/java/com/kazale/pontointeligente/api/services/ExemploService.java
@@ -0,0 +1,12 @@
+package com.kazale.pontointeligente.api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+
+    public void testarServico() {
+        System.out.println("### Executando serviço de teste!!!");
+    }
+
+}

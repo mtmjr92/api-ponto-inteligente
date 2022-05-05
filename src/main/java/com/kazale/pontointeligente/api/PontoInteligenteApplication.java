004c123f8fa4f439e216a12e7b697b8883499edc
mtmjr92 <>
2022-05-04 23:10:16 -0300

    17. Gerando senhas com o BCrypt

--- src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,5 +1,6 @@
 package com.kazale.pontointeligente.api;
 
+import com.kazale.pontointeligente.api.utils.SenhaUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +21,16 @@ public class PontoInteligenteApplication {
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			System.out.println("### Quantidade de elementos por pagina = " + this.qntPorPagina);
+
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+
+			System.out.println("Senha encoded: " + senhaEncoded);
+
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+
+			System.out.println("Senha encoded: " + senhaEncoded);
+
+			System.out.println("Senha valida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
 		};
 	}
 

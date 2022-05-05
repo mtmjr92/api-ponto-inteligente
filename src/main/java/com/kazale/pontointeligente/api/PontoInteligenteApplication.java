781ea787ebc343ac459a47915fe387be0ac0aa61
mtmjr92 <>
2022-05-04 21:36:12 -0300

    15. Parâmetros de configuração (application.properties)

--- src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,13 +1,25 @@
 package com.kazale.pontointeligente.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PontoInteligenteApplication {
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qntPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por pagina = " + this.qntPorPagina);
+		};
+	}
 }
